d2b4fa0eb5bd95152918b15f3ef91d05375bb7e8
Michael Melancon <mike@ultix.com>
2014-03-10 15:47:04 -0500

    Added alias package.

--- DictionarySuggestDemo/ViewModels/SearchViewModel.cs
+++ DictionarySuggestDemo/ViewModels/SearchViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Observable.Aliases;
 using System.Threading.Tasks;
 
 namespace DictionarySuggestDemo.ViewModels
